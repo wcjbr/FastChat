9fa64054b34069fb74426acca47f2e47b351d027
wcjbr <wcjpython@qq.com>
2026-08-07 20:15:04 +0800

    fix windows

--- lib/acgo/acgo_http_client_io.dart
+++ lib/acgo/acgo_http_client_io.dart
@@ -1,33 +1,9 @@
-import 'dart:io';
-
 import 'package:acgo_sdk/acgo_sdk.dart';
 
 class FastChatAcgoHttp {
   const FastChatAcgoHttp._();
 
   static AcgoClient createClient({String? accessToken}) {
-    return HttpOverrides.runWithHttpOverrides(
-      () => AcgoClient(accessToken: accessToken),
-      _AcgoHttpOverrides(),
-    );
-  }
-
-  static bool _isAcgoHost(String host) {
-    final normalized = host.toLowerCase();
-    return normalized == 'acgo.cn' ||
-        normalized.endsWith('.acgo.cn') ||
-        normalized == 'xiaomawang.com' ||
-        normalized.endsWith('.xiaomawang.com');
-  }
-}
-
-class _AcgoHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    final client = super.createHttpClient(context);
-    client.badCertificateCallback = (certificate, host, port) {
-      return FastChatAcgoHttp._isAcgoHost(host);
-    };
-    return client;
+    return AcgoClient(accessToken: accessToken);
   }
 }
